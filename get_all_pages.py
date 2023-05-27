975b5ed6030d07727ab9d52579e70b748b7ae045
Evgenii Sergunin <mckrei@ya.ru>
2023-05-27 17:03:33 +0300

    удалили мути

--- get_all_pages.py
+++ get_all_pages.py
@@ -1,4 +1,3 @@
-import multiprocessing
 import re
 import time
 
@@ -11,29 +10,24 @@ def save_page(page_data, name):
 
 
 def get_url():
-    with open('unique_phones.csv', 'r', encoding='utf-8') as f:
+    with open('unique.csv', 'r', encoding='utf-8') as f:
         result = [el.split('|')[0] for el in f.read().strip().split('\n')]
     return result
 
 
 def search_and_save_data(urls):
+    parsing = Parsing()
     for url in urls:
-        parsing = Parsing()
         page = parsing.get_page('https://www.ozon.ru' + url)
         name = re.findall(r'\d+', url)[-1]
         save_page(page, name)
-        time.sleep(1)
-        parsing.driver.quit()
+    parsing.driver.quit()
 
 
-def main(process=10):
+def main():
     all_url = get_url()
-    chunk_size = len(all_url) // process
-    chunks = [all_url[i:i+chunk_size] for i in range(0, len(all_url), chunk_size)]
-    pool = multiprocessing.Pool(processes=process)
-    pool.map(search_and_save_data, chunks)
-    pool.close()
-    pool.join()
+    # chunks = [all_url[i:i+chunk_size] for i in range(0, len(all_url), chunk_size)]
+    search_and_save_data(all_url[:50])
 
 if __name__ == '__main__':
     main()
