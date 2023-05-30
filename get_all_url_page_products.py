bb3df153ff57a144a9d81edc555d69153ea1624d
Evgenii Sergunin <mckrei@ya.ru>
2023-05-30 14:17:09 +0300

    er

--- get_all_url_page_products.py
+++ get_all_url_page_products.py
@@ -1,26 +1,27 @@
 from bs4 import BeautifulSoup
 
+import sys
 from main import Parsing
 
 
 def get_all_data_page(page_source: str) -> list:
     soup = BeautifulSoup(page_source, "lxml")
     all_data = []
-    blocks = soup.find_all('div', {'class': 'im0 i0m'})
+    blocks = soup.find_all('div', {'class': 'i2m mi2'})
     print(len(blocks))
     for block in blocks:
         if not block:
             continue
-        url = block.find('a', {'class': 'tile-hover-target ii8 i9i'})
+        url = block.find('a', {'class': 'tile-hover-target j0i ji1'})
         all_data.append((
-            block.find('span', {'class': 'dt5 td5 dt6 dt8 tsBodyL ii8 i9i'}).text,
+            block.find('span', {'class': 'd7t t7d d8t ud tsBodyL j0i ji1'}).text,
             url.get('href') if url else None,
         ))
     return all_data
 
 
 def save_data_to_csv(data) -> list[list]:
-    with open("obuv.csv", "a", encoding="utf-8") as file:
+    with open("kappa.csv", "a", encoding="utf-8") as file:
         for item in data:
             file.write('|'.join(map(str, item)) + '\n')
 
@@ -31,11 +32,11 @@ columns = [
 ]
 
 
-def main():
+def main(start, end):
     save_data_to_csv([columns])
-    url = "https://www.ozon.ru/brand/nike-18077713/category/obuv-17777?page="
+    url = "https://www.ozon.ru/brand/kappa-146870879/category/odezhda-obuv-i-aksessuary-7500/?page="
     parsing = Parsing()
-    for i in range(1, 279):
+    for i in range(start, end):
         print(i)
         page = parsing.get_page(f'{url}{i}')
         data = get_all_data_page(page)
@@ -43,4 +44,5 @@ def main():
     parsing.driver.quit()
 
 if __name__ == '__main__':
-    main()
+    start, end = map(int, sys.argv[1:])
+    main(start, end)
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import time
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import undetected_chromedriver.v2 as uc
 
 
 class Parsing:
     '''Класс для сохранения состояния драйвера и получения html страницы'''
     def __init__(self):
-        options = Options()
-        options.add_argument('--user-data-dir=/tmp/profile2')  # Изменяем путь к профилю на Linux-совместимый
+        options = uc.ChromeOptions()
+        # options.add_argument('--user-data-dir=c:\\temp\\profile2')
         options.add_argument('--no-first-run --no-service-autorun --password-store=basic')
 
         self.driver = uc.Chrome(options=options)
@@ -16,8 +14,8 @@ class Parsing:
     def get_page(self, url):
         '''Получаем html страницы'''
         self.driver.get(url)
-        time.sleep(3)
+        time.sleep(2)
         self.driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
-        time.sleep(1)
+        time.sleep(4)
         page_source = self.driver.page_source
         return page_source
